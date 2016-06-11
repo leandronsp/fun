945610eff13d3763085f4f77e08b0b554b3e6a0e
Leandro Maríngolo <leandronsp@gmail.com>
2016-06-11 22:00:28 +0100

    finish basic data types

--- learning_elixir/test/basic_arithmetic_test.exs
+++ learning_elixir/test/basic_arithmetic_test.exs
@@ -13,4 +13,37 @@ defmodule LETest do
   test "`rem` function returns integer" do
     assert rem(10, 3) == 1
   end
+
+  test "true/false are atoms" do
+    assert is_atom(true) == true
+    assert is_boolean(true) == true
+  end
+
+  test "string interpolation" do
+    name = "Leandro"
+    assert "Hello, #{name}" == "Hello, Leandro"
+  end
+
+  test "anonymous funcions are clojures and do not affect environment" do
+    name = "First"
+
+    add = fn(a, b) ->
+      name = "Second"
+      a + b
+    end
+
+    assert add.(2, 3) == 5
+    assert name == "First"
+  end
+
+  test "`Lists` are like linked lists" do
+    list = [1 | [2 | [3 | []]]]
+    assert list == [1, 2, 3]
+    assert length(list) == 3
+  end
+
+  test "`Tuples` are like arrays" do
+    tuple = { "1", "2", "3" }
+    assert tuple_size(tuple) == 3
+  end
 end
