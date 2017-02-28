ace9709b328da382bc7ec41743f04eaef40abdfa
Leandro Maríngolo <leandronsp@gmail.com>
2017-02-28 00:47:33 +0000

    first exercise

--- exercism/README.md
+++
@@ -1,5 +0,0 @@
-# Exercism
-
-**Exercises from http://exercism.io/ using Elixir**
-
-[My profile](http://exercism.io/leandronsp)
---
+++ exercism/elixir/hello-world/hello_world_test.exs
@@ -0,0 +1,25 @@
+if !System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("hello_world.exs", __DIR__)
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule HelloWorldTest do
+  use ExUnit.Case
+
+  test "says hello with no name" do
+    assert HelloWorld.hello() == "Hello, World!"
+  end
+
+  @tag :pending
+  test "says hello sample name" do
+    assert HelloWorld.hello("Alice") == "Hello, Alice!"
+  end
+
+  @tag :pending
+  test "says hello other sample name" do
+    assert HelloWorld.hello("Bob") == "Hello, Bob!"
+  end
+
+end
