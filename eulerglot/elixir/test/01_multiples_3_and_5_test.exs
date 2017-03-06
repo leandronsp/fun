05c313c63e5f1a06e9bec3608d534338e41c5f14
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-06 23:52:25 +0000

    euler 01 elixir

---
+++ eulerglot/elixir/test/01_multiples_3_and_5_test.exs
@@ -0,0 +1,4 @@
+defmodule Multiples3And5Test do
+  use ExUnit.Case
+  doctest Multiples3And5
+end
--- eulerglot/elixir/test/elixir_euler_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ElixirEulerTest do
-  use ExUnit.Case
-  doctest ElixirEuler
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
