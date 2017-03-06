d999d476c0f18fcfcf6855803ac2368a14ecdba7
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-06 21:18:02 +0000

    euler elixir

---
+++ eulerglot/elixir/lib/elixir_euler.ex
@@ -0,0 +1,18 @@
+defmodule ElixirEuler do
+  @moduledoc """
+  Documentation for ElixirEuler.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirEuler.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ eulerglot/elixir/test/elixir_euler_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirEulerTest do
+  use ExUnit.Case
+  doctest ElixirEuler
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
