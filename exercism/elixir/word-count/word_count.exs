10fc007ac3536ae78128f22bd273cfed0db531cb
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-06 23:07:30 +0000

    exercism

---
+++ exercism/elixir/word-count/word_count.exs
@@ -0,0 +1,11 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map
+  def count(sentence) do
+
+  end
+end
