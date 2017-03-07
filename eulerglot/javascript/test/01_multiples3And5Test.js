4e324005b4ad90bf7ad6727a6683d28577f8eee3
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-07 00:20:26 +0000

    refactoring javascript

--- eulerglot/javascript/test/01_multiples3And5Test.js
+++ eulerglot/javascript/test/01_multiples3And5Test.js
@@ -2,21 +2,33 @@ var assert = require('assert');
 
 function buildArray(arr, floor, ceil) {
   arr.push(floor)
-  if (floor == (ceil - 1)) {
+  if (floor == ceil) {
     return arr;
   }
   return buildArray(arr, floor + 1, ceil);
 }
 
 function sumUnder(ceil) {
-  array     = buildArray([], 1, ceil);
-  multiples = array.filter(function(number) {
-    return (number % 3) == 0 || (number % 5) == 0
-  });
+  array = buildArray([], 1, ceil - 1);
+  return sumMultiples(array, 0)
+}
 
-  return multiples.reduce(function(acc, number) {
-    return acc + number;
-  });
+function sumMultiples(array, acc) {
+  if (array.length == 0) {
+    return acc;
+  }
+
+  number = array[array.length - 1]
+
+  if (number % 3 == 0 || number % 5 == 0) {
+    return sumMultiples(tail(array), number + acc);
+  }
+
+  return sumMultiples(tail(array), acc);
+}
+
+function tail(array) {
+  return array.slice(0, array.length - 1);
 }
 
 describe('multiples of 3 and 5', function() {
