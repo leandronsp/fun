229baea292ed8e7345bb3bc9d9d75d45b1c32940
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-07 00:03:37 +0000

    refactor ruby

--- eulerglot/ruby/lib/001_multiples_3_and_5.rb
+++ eulerglot/ruby/lib/001_multiples_3_and_5.rb
@@ -1,11 +1,23 @@
 class Multiples3And5
-  def self.under(ceil)
-    (1...ceil).select do |number|
-      number % 5 == 0 || number % 3 == 0
-    end
-  end
 
   def self.sum_under(ceil)
-    under(ceil).reduce(:+)
+    sum((1..ceil - 1).to_a, 0)
   end
+
+  private
+
+  def self.sum(array, acc)
+    return acc if array == []
+
+    number = array.pop
+
+    if number % 3 == 0 || number % 5 == 0
+      result = number + acc
+    else
+      result = acc
+    end
+
+    sum(array, result)
+  end
+
 end
