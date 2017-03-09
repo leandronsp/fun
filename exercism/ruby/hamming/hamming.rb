3f2416d299f2511af39b1246a8e7044234974d82
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-09 12:06:36 +0000

    hamming difference algorithm in ruby

---
+++ exercism/ruby/hamming/hamming.rb
@@ -0,0 +1,23 @@
+class Hamming
+  def self.compute(strand_a, strand_b)
+    chars_a, chars_b = strand_a.chars, strand_b.chars
+    raise ArgumentError if chars_a.length != chars_b.length
+
+    do_compute(chars_a, chars_b, 0)
+  end
+
+  private
+
+  def self.do_compute(chars_a, chars_b, acc)
+    return acc if chars_a.length == 0
+
+    char_a, char_b = chars_a.shift, chars_b.shift
+    diff = difference(char_a, char_b)
+
+    do_compute(chars_a, chars_b, acc + diff)
+  end
+
+  def self.difference(char_a, char_b)
+    char_a == char_b ? 0 : 1
+  end
+end
--- exercism/ruby/hamming/hamming_test.rb
+++ exercism/ruby/hamming/hamming_test.rb
@@ -3,82 +3,71 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'hamming'
 
+module BookKeeping
+  VERSION = 3
+end
+
 # Test data version:
 # deb225e Implement canonical dataset for scrabble-score problem (#255)
 
 class HammingTest < Minitest::Test
   def test_identical_strands
-    # skip
     assert_equal 0, Hamming.compute('A', 'A')
   end
 
   def test_long_identical_strands
-    skip
     assert_equal 0, Hamming.compute('GGACTGA', 'GGACTGA')
   end
 
   def test_complete_distance_in_single_nucleotide_strands
-    skip
     assert_equal 1, Hamming.compute('A', 'G')
   end
 
   def test_complete_distance_in_small_strands
-    skip
     assert_equal 2, Hamming.compute('AG', 'CT')
   end
 
   def test_small_distance_in_small_strands
-    skip
     assert_equal 1, Hamming.compute('AT', 'CT')
   end
 
   def test_small_distance
-    skip
     assert_equal 1, Hamming.compute('GGACG', 'GGTCG')
   end
 
   def test_small_distance_in_long_strands
-    skip
     assert_equal 2, Hamming.compute('ACCAGGG', 'ACTATGG')
   end
 
   def test_non_unique_character_in_first_strand
-    skip
     assert_equal 1, Hamming.compute('AGA', 'AGG')
   end
 
   def test_non_unique_character_in_second_strand
-    skip
     assert_equal 1, Hamming.compute('AGG', 'AGA')
   end
 
   def test_same_nucleotides_in_different_positions
-    skip
     assert_equal 2, Hamming.compute('TAG', 'GAT')
   end
 
   def test_large_distance
-    skip
     assert_equal 4, Hamming.compute('GATACA', 'GCATAA')
   end
 
   def test_large_distance_in_off_by_one_strand
-    skip
     assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
   end
 
   def test_empty_strands
-    skip
     assert_equal 0, Hamming.compute('', '')
   end
 
   def test_disallow_first_strand_longer
-    skip
     assert_raises(ArgumentError) { Hamming.compute('AATG', 'AAA') }
   end
 
   def test_disallow_second_strand_longer
-    skip
     assert_raises(ArgumentError) { Hamming.compute('ATA', 'AGTG') }
   end
 
@@ -99,7 +88,6 @@ class HammingTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 3, BookKeeping::VERSION
   end
 end
