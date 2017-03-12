1428ed5197a7110893e511a87cb928b343dadc0c
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-12 20:56:48 +0000

    better documentation

--- eulerglot/elixir/lib/001_multiples_3_and_5.ex
+++ eulerglot/elixir/lib/001_multiples_3_and_5.ex
@@ -1,8 +1,16 @@
 defmodule Multiples3And5 do
   @moduledoc """
-  If we list all the natural numbers below 10 that are multiples of 3 or 5,
-  we get 3, 5, 6 and 9. The sum of these multiples is 23.
-  Find the sum of all the multiples of 3 or 5 below 1000.
+  ## Problem
+    If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+  ## Solution
+    By using arithmetic progression our time complexity is O(1).
+    Sum all multiples of 3 by multiples of 5, and to avoid repetition on 15 (because
+    3 x 5), subtract the multiples of 15:
+      Count = Floor(n / m), where n is the max and m is the multiple
+      Sum = ((1 + Count) / 2) * Count
   """
 
   def sum_under(ceil) do
