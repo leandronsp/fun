183e47c0350679b25bbbe5827efd72bd9fe0c01a
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-12 01:05:04 +0000

    gigasecond in Ruby

---
+++ exercism/ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,5 @@
+class Gigasecond
+  def self.from(time)
+    time + (10 ** 9)
+  end
+end
---
+++ exercism/ruby/gigasecond/gigasecond_test.rb
@@ -0,0 +1,62 @@
+require 'minitest/autorun'
+require_relative 'gigasecond'
+
+class BookKeeping
+  VERSION = 5
+end
+
+# Test data version: 9049dc7
+
+class GigasecondTest < Minitest::Test
+  def test_2011_04_25
+    result = Gigasecond.from(Time.utc(2011, 4, 25, 0, 0, 0))
+    assert_equal Time.utc(2043, 1, 1, 1, 46, 40), result
+  end
+
+  def test_1977_06_13
+    result = Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))
+    assert_equal Time.utc(2009, 2, 19, 1, 46, 40), result
+  end
+
+  def test_1959_07_19
+    result = Gigasecond.from(Time.utc(1959, 7, 19, 0, 0, 0))
+    assert_equal Time.utc(1991, 3, 27, 1, 46, 40), result
+  end
+
+  def test_full_time_specified
+    result = Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
+    assert_equal Time.utc(2046, 10, 2, 23, 46, 40), result
+  end
+
+  def test_full_time_with_day_roll_over
+    result = Gigasecond.from(Time.utc(2015, 1, 24, 23, 59, 59))
+    assert_equal Time.utc(2046, 10, 3, 1, 46, 39), result
+  end
+
+  # Test your 1Gs anniversary
+  def test_with_your_birthday
+    result = Gigasecond.from(Time.utc(1986, 12, 15, 05, 00, 00))
+    assert_equal Time.utc(2018, 8, 23, 6, 46, 40), result
+  end
+
+  # Problems in exercism evolve over time, as we find better ways to ask
+  # questions.
+  # The version number refers to the version of the problem you solved,
+  # not your solution.
+  #
+  # Define a constant named VERSION inside of the top level BookKeeping
+  # module, which may be placed near the end of your file.
+  #
+  # In your file, it will look like this:
+  #
+  # module BookKeeping
+  #   VERSION = 1 # Where the version number matches the one in the test.
+  # end
+  #
+  # If you are curious, read more about constants on RubyDoc:
+  # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
+
+  def test_bookkeeping
+    assert_equal 5, BookKeeping::VERSION
+  end
+end
