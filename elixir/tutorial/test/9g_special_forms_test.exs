c8c8279d1177cf253b4a7f141152a251e3128589
Leandro Maríngolo <leandronsp@gmail.com>
2017-03-29 23:48:20 +0100

    TIL more special forms in Elixir!

---
+++ elixir/tutorial/test/9g_special_forms_test.exs
@@ -0,0 +1,22 @@
+defmodule SpecialFormsTest do
+  use ExUnit.Case
+
+  def split_string(<<>>, acc), do: acc
+  def split_string(<<head, tail::binary>>, acc) do
+    split_string(tail, acc ++ [<<head>>])
+  end
+
+  def split_string_in_chunks(<<>>, acc), do: acc
+  def split_string_in_chunks(<<head::binary-size(3), tail::binary>>, acc) do
+    split_string_in_chunks(tail, acc ++ [head])
+  end
+
+  test "split word" do
+    assert split_string("abc", []) == ~w(a b c)
+  end
+
+  test "split word in chunks of 3" do
+    assert split_string_in_chunks("abcdefghi", []) == ~w(abc def ghi)
+  end
+
+end
