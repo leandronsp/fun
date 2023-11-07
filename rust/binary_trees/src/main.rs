82d93d4ade451dbfc647748f50f67043c24ae918
leandronsp <leandronsp@gmail.com>
2023-11-06 23:27:41 -0300

    (Rust) implement binary tree inversion using recursion

--- rust/binary_trees/src/main.rs
+++ rust/binary_trees/src/main.rs
@@ -9,7 +9,22 @@ fn main() {
        right: Option<Rc<RefCell<Node>>>,
    } 
 
-   fn invert_tree(tree: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
+   fn invert_tree_using_recursion(tree: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
+      let mut borrowed_tree = tree.borrow_mut();
+      let temp_left = borrowed_tree.left.take();
+
+      if let Some(thing) = borrowed_tree.right.take() {
+         borrowed_tree.left = Some(invert_tree_using_recursion(thing))
+      }
+
+      if let Some(thing) = temp_left {
+         borrowed_tree.right = Some(invert_tree_using_recursion(thing))
+      }
+
+      tree.clone()
+   }
+
+   fn invert_tree_using_stack(tree: Rc<RefCell<Node>>) {
       // LIFO
       let mut stack: Vec<Rc<RefCell<Node>>> = Vec::new();
       stack.push(tree.clone());
@@ -31,32 +46,33 @@ fn main() {
             stack.push(right.clone());
          }
       }
+   }
+
+   fn build_tree() -> Rc<RefCell<Node>> {
+      let tree = Rc::new(RefCell::new(Node { value: 1, left: None, right: None }));
+
+      tree.borrow_mut().left = Some(Rc::new(RefCell::new(Node{ value: 2, left: None, right: None })));
+      tree.borrow_mut().right = Some(Rc::new(RefCell::new(Node{ value: 3, left: None, right: None })));
+
+      // tree.left.left
+      tree.borrow_mut().left.clone().unwrap().borrow_mut().left = Some(Rc::new(RefCell::new(Node { value: 4, left: None, right: None })));
+
+      // tree.right.right
+      tree.borrow_mut().right.clone().unwrap().borrow_mut().right = Some(Rc::new(RefCell::new(Node { value: 5, left: None, right: None })));
 
       tree
    }
 
-   // create tree
-   let tree = Rc::new(RefCell::new(Node { value: 1, left: None, right: None }));
+   let tree = build_tree();
+
    assert_eq!(1, tree.borrow().value);
-   assert_eq!(None, tree.borrow().left);
-   assert_eq!(None, tree.borrow().right);
-
-   // tree.left
-   tree.borrow_mut().left = Some(Rc::new(RefCell::new(Node{ value: 2, left: None, right: None })));
    assert_eq!(2, tree.borrow().left.clone().unwrap().borrow().value);
-
-   // tree.right
-   tree.borrow_mut().right = Some(Rc::new(RefCell::new(Node{ value: 3, left: None, right: None })));
    assert_eq!(3, tree.borrow().right.clone().unwrap().borrow().value);
-
-   // tree.left.left
-   tree.borrow_mut().left.clone().unwrap().borrow_mut().left = Some(Rc::new(RefCell::new(Node { value: 4, left: None, right: None })));
    assert_eq!(4, tree.borrow().left.clone().unwrap().borrow().left.clone().unwrap().borrow().value);
-
-   // tree.right.right
-   tree.borrow_mut().right.clone().unwrap().borrow_mut().right = Some(Rc::new(RefCell::new(Node { value: 5, left: None, right: None })));
    assert_eq!(5, tree.borrow().right.clone().unwrap().borrow().right.clone().unwrap().borrow().value);
 
+   // Invert a binary tree
+   //
    // At this moment, we have this tree
    // # Input:
    //       1
@@ -73,11 +89,23 @@ fn main() {
    //    /     \
    //   5       4
 
-   let inverted_tree = invert_tree(tree.clone());
+   // Using stack
+   let tree = build_tree();
+   invert_tree_using_stack(tree.clone());
 
-   assert_eq!(1, inverted_tree.borrow().value);
-   assert_eq!(3, inverted_tree.borrow().left.clone().unwrap().borrow().value);
-   assert_eq!(5, inverted_tree.borrow().left.clone().unwrap().borrow().left.clone().unwrap().borrow().value);
-   assert_eq!(2, inverted_tree.borrow().right.clone().unwrap().borrow().value);
-   assert_eq!(4, inverted_tree.borrow().right.clone().unwrap().borrow().right.clone().unwrap().borrow().value);
+   assert_eq!(1, tree.borrow().value);
+   assert_eq!(3, tree.borrow().left.clone().unwrap().borrow().value);
+   assert_eq!(5, tree.borrow().left.clone().unwrap().borrow().left.clone().unwrap().borrow().value);
+   assert_eq!(2, tree.borrow().right.clone().unwrap().borrow().value);
+   assert_eq!(4, tree.borrow().right.clone().unwrap().borrow().right.clone().unwrap().borrow().value);
+
+   // Using recursion
+   let tree = build_tree();
+   invert_tree_using_recursion(tree.clone());
+
+   assert_eq!(1, tree.borrow().value);
+   assert_eq!(3, tree.borrow().left.clone().unwrap().borrow().value);
+   assert_eq!(5, tree.borrow().left.clone().unwrap().borrow().left.clone().unwrap().borrow().value);
+   assert_eq!(2, tree.borrow().right.clone().unwrap().borrow().value);
+   assert_eq!(4, tree.borrow().right.clone().unwrap().borrow().right.clone().unwrap().borrow().value);
 }
